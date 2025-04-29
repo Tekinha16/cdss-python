081f2e72ea84776aab5f0c6829439fbb8106eb01
tekinha16 <00001116025887sp@al.educacao.sp.gov.br>
2025-04-29 11:56:24 -0300

    criando funções de soma

---
+++ function.py
@@ -0,0 +1,16 @@
+def soma():
+    nota01 = 2
+    nota02 = 1
+
+    resultado = nota01 + nota02
+
+    return print(resultado)
+
+# soma()
+
+def somardoisnumeros(numero01,numero02):
+    resultado = numero01 + numero02
+
+    return print(resultado)
+
+somardoisnumeros(6, 8)
